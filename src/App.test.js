7ae8b44aa4024b7cb5009dcb2154bd2e6babca26
guerrier <guerrier@live.co.uk>
2019-07-29 15:30:10 -0700

    [FEAT]: Test removeTodo
    -> Add test to remove todo.
    -> Ensure when click complete not remove and vice versa

--- src/App.test.js
+++ src/App.test.js
@@ -31,6 +31,34 @@ describe("App", () => {
         .simulate('click');
 
       expect(completeTodo.mock.calls).toEqual([[5]]);
+      expect(removeTodo.mock.calls).toEqual([]);
+    });
+
+
+    it('Ejecuta removeTodo cuando pincho X', () => {
+      const completeTodo = jest.fn();
+      const removeTodo = jest.fn();
+      const index = 5;
+      const todo = {
+        isCompleted: true,
+        text: 'lala'
+      };
+
+      const wrapper = shallow(
+        <Todo
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}
+          index={index}
+          todo={todo}
+        />
+      );
+      wrapper
+        .find('button')
+        .at(1)
+        .simulate('click');
+
+      expect(removeTodo.mock.calls).toEqual([[5]]);
+      expect(completeTodo.mock.calls).toEqual([]);
     });
   });
 });
