fdac5d0dda804f8831d8810f97194769056b9a73
guerrier <guerrier@live.co.uk>
2019-07-30 12:31:03 -0700

    [FEAT]: Add test for completeTodo and removeTodo.

--- src/App.test.js
+++ src/App.test.js
@@ -79,6 +79,7 @@ describe("App", () => {
   });
 
   describe('custom hook: useTodos', () => {
+    
     it('addTodo', () => {
       const Test = (props) => {
         const hook = props.hook();
@@ -90,6 +91,41 @@ describe("App", () => {
       props.addTodo('Texto de prueba');
       props = wrapper.find('div').props();
       expect(props.todos[0]).toEqual({ text: 'Texto de prueba'});
+    });
+
+    it('completeTodo', () => {
+      const Test = (props) => {
+        const hook = props.hook();
+        return <div {...hook}/>;
+      };
+
+      const wrapper = shallow(<Test hook={useTodos}/>);
+      let props = wrapper.find('div').props();
+      props.completeTodo(0);
+      props = wrapper.find('div').props();
+      expect(props.todos[0]).toEqual({ text: 'Todo 1', isCompleted: true });
+    });
+
+    it('removeTodo', () => {
+      const Test = (props) => {
+        const hook = props.hook();
+        return <div {...hook}/>;
+      };
+
+      const wrapper = shallow(<Test hook={useTodos}/>);
+      let props = wrapper.find('div').props();
+      props.removeTodo(0);
+      props = wrapper.find('div').props();
+      expect(props.todos).toEqual([
+        {
+          text: "Todo 2",
+          isCompleted: false
+        },
+        {
+          text: "Todo 3",
+          isCompleted: false
+        }
+      ]);
     });
   });
 });
